cb129fbbc7522583f444373352a00291c54a6c40
yijaejun <yijaejun81@gmail.com>
2018-02-19 14:42:47 +0900

    no message

--- test/mocha/testSpec.js
+++ test/mocha/testSpec.js
@@ -2,15 +2,12 @@ import { assert } from 'chai';
 import $ from 'jquery';
 // import simpleCarousel from '../../assets/javascript/SimpleCarousel';
 
-
 // TODO UI테스트는 어떻게 할 것인가?
 // TODO 제이쿼리 플러그인을 제대로 테스트하려면?
 
 describe('Describe test ', () => {
 	
 	it('test sub test', (done) => {
-		console.info($);
-		
 		done();
 	});
 	
